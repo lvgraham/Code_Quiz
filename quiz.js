55ad11d0b950bafb2fa7e567f710b0ab42bb590b
lvgraham <liavibanez@gmail.com>
2020-05-17 10:40:40 -0500

    adding timer element

--- quiz.js
+++ quiz.js
@@ -17,8 +17,33 @@ nextButton.addEventListener('click', () => {
 
 resultsButton.addEventListener('click', showResults());
 
+//timer functions
+document.getElementById('timer').innerHTML =
+  005 + ":" + 00;
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
+  
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  console.log(m)
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
+}
+
 function startGame(){
     console.log('started');
+    startTimer();
     startButton.classList.add('hide');
     shuffledQuestions = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
