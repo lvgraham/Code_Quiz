f18b500953cd4dc55426cf693c41ccc05eb8e3fb
lvgraham <liavibanez@gmail.com>
2020-05-17 09:04:53 -0500

    adding restart css

--- quiz.js
+++ quiz.js
@@ -59,7 +59,8 @@ function selectAnswer(e){
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide');
     } else {
-        startButton.innerText = "restart"
+        startButton.innerText = "RESTART"
+        startButton.classList.add('restart');
         startButton.classList.remove('hide');
     }
 }
